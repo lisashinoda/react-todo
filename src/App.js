644e6f7f65a40258808d61bc3faa5344b52b2e49
lisashinoda <lisa.621.nokonoko@gmail.com>
2020-07-19 22:42:23 +0900

    add delete function

--- src/App.js
+++ src/App.js
@@ -9,11 +9,20 @@ class App extends Component{
       {id:2, content:'play mario cart'}
     ]
   }
+  deleteTodo =(id)=>{
+    const todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    });
+    this.setState({
+        todos
+    })
+    
+  }
   render(){
     return(
-      <div className="App">
+      <div className="todo-app container">
         <h1 className="center-blue-text">Todo's</h1>
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
